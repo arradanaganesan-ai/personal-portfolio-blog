585544babc171afb5fe2009f2d5e35162ea4e6d3
arradanaganesan-ai <arradanaganesan@gmail.com>
2026-06-07 11:01:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ button.addEventListener("click", () => {
         message.style.display = "none";
     }
 });
+function loadUsers(){
+
+fetch("https://jsonplaceholder.typicode.com/users")
+
+.then(response => response.json())
+
+.then(data => {
+
+let output = "";
+
+data.forEach(user => {
+
+output += `
+<div class="card">
+<h3>${user.name}</h3>
+<p>${user.email}</p>
+<p>${user.address.city}</p>
+</div>
+`;
+
+});
+
+document.getElementById("users").innerHTML = output;
+
+})
+
+.catch(error => console.log(error));
+
+}
