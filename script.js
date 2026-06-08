59207af825739cdeb0be0c46e8478543a148c1af
arradanaganesan-ai <arradanaganesan@gmail.com>
2026-06-08 13:02:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -37,3 +37,43 @@ document.getElementById("users").innerHTML = output;
 .catch(error => console.log(error));
 
 }
+
+document.getElementById("contactForm").addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    document.getElementById("nameError").textContent = "";
+    document.getElementById("emailError").textContent = "";
+    document.getElementById("messageError").textContent = "";
+
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let message = document.getElementById("userMessage").value.trim();
+
+    let valid = true;
+
+    if(name === ""){
+        document.getElementById("nameError").textContent = "Name is required";
+        valid = false;
+    }
+
+    let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if(email === ""){
+        document.getElementById("emailError").textContent = "Email is required";
+        valid = false;
+    }
+    else if(!email.match(emailPattern)){
+        document.getElementById("emailError").textContent = "Enter a valid email";
+        valid = false;
+    }
+
+    if(message === ""){
+        document.getElementById("messageError").textContent = "Message is required";
+        valid = false;
+    }
+
+    if(valid){
+        alert("Form Submitted Successfully!");
+    }
+});
