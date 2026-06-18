eddd10428498ad4c0d6682b324584666818ec6cc
Arradana Ganesan <arradanaganesan@gmail.com>
2026-06-18 11:14:22 +0530

    Added JWT authentication and protected routes

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 dotenv.config();
+
+const prisma = new PrismaClient();
 
 const app = express();
 
@@ -82,7 +87,90 @@ app.delete("/users/:id", (req, res) => {
 
 });
 
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    res.json({
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  console.log("AUTH HEADER:", authHeader);
+
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Access denied",
+    });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({
+        message: "Invalid token",
+      });
+    }
+
+    req.user = user;
+    next();
+  });
+}
+app.get("/profile", authenticateToken, (req, res) => {
+  res.json({
+    message: "Welcome to protected route",
+    user: req.user,
+  });
+});
+console.log("PROFILE ROUTE LOADED");
 // Start server
 
 app.listen(PORT, () => {
