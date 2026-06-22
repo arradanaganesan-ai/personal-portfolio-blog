fc0c2bea5d1a5406e373489979b231a2d0b26485
Arradana Ganesan <arradanaganesan@gmail.com>
2026-06-22 12:56:32 +0530

    fix cors module

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { PrismaClient } = require("@prisma/Client");
+const { PrismaClient } = require("@prisma/client");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
 
 
 // Normal pages
